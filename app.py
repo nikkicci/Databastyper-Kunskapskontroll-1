f5ef7b90cd947d55f2d382fbe17e5307d7bd6851
nikki <nikki@example.com>
2025-02-12 17:03:27 +0100

    updated app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ merged_data = pd.merge(products_df, suppliers_df, on='SupplierID', how='inner')
 username = st.secrets["mongo"]["username"] 
 PWD = st.secrets["mongo"]["password"]
 encoded_pwd = urllib.parse.quote_plus(PWD)
-uri = f'mongodb+srv://nicolinalottsfeldtdata24hel:{encoded_pwd}@cluster0.xljmi.mongodb.net/'
+uri = f'mongodb+srv://{username}:{encoded_pwd}@cluster0.xljmi.mongodb.net/'
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 
